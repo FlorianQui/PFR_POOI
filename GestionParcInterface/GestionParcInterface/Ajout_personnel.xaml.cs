963190e51d458b3d7206f661c3f403fca904e99d
GaetanRobic <gaetan.robic@devinci.fr>
2018-05-17 16:02:45 +0200

    ajout_personnel

--- GestionParcInterface/GestionParcInterface/Ajout_personnel.xaml.cs
+++ GestionParcInterface/GestionParcInterface/Ajout_personnel.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,5 +25,50 @@ namespace GestionParcInterface
         {
             InitializeComponent();
         }
+        private void button_sorcier_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (PropertyDescriptor descriptor in TypeDescriptor.GetProperties(typeof(Sorcier)))
+            {              
+                StackPanel stackPanel = new StackPanel();
+                stackPanel.Orientation = Orientation.Vertical;
+                stackPanel.Background = Brushes.White; 
+
+                TextBlock textBlock = new TextBlock();
+                textBlock.Text = descriptor.Name +"  ";
+                stackPanel.Children.Add(textBlock);
+
+                TextBox textBox = new TextBox();
+                stackPanel.Children.Add(textBox);
+                ajout.Children.Add(stackPanel);
+            }
+            Button validation = new Button();
+            ajout.Children.Add(validation);
+            //ajout.header
+        }
+        private void button_monstres_Click(object sender, RoutedEventArgs e)
+        {
+           
+        }
+
+        private void button_demons_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button_zombies_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button_loup_garou_Click(object sender, RoutedEventArgs e)
+        {
+          
+        }
+
+        private void button_fantomes_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+       
     }
 }
